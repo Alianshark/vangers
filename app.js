2d88e638f4e262ae381d189868ffa797d56b3a6e
Enormouse <johnnysalvia@gmail.com>
2023-03-13 23:09:24 +0200

    Create 3 planets with gravity

---
+++ app.js
@@ -0,0 +1,72 @@
+let mars = {
+  x: 150,
+  y: 100,
+  Vx: 0,
+  Vy: 0,
+  Ma: 1,
+  star: document.querySelector('#mars'),
+}
+
+let earth = {
+  x: 768, //earth start position x
+  y: 432, //start position y
+  Vx: 0, // Speed 
+  Vy: 0, // Speed
+  Ma: 1000, //Mass
+  star: document.querySelector('#earth'),
+}
+let moon = {
+  x: 400,
+  y: 300,
+  Vx: 10, //speed
+  Vy: 0, //speed
+  Ma: 1, //Mass
+  star: document.querySelector('#moon'),
+}
+
+let distX = earth.x-moon.x;
+let distY = earth.y-moon.y;
+let dist = Math.sqrt(distX * distX + distY * distY);
+
+let Fx = 0; //force
+let Fy = 0; //force
+const G = 0.1; // G-constat
+
+mars.star.style.position = 'absolute';
+moon.star.style.position = 'absolute';
+earth.star.style.position = 'absolute';
+
+setInterval(time, 1000/60);
+
+function time () {
+  distX = earth.x-moon.x;
+  distY = earth.y-moon.y;
+  dist = Math.sqrt(distX * distX + distY * distY);
+
+  Fx = distX * G * earth.Ma * moon.Ma / (dist * dist) ;
+  Fy = distY * G * earth.Ma * moon.Ma / (dist * dist) ;
+    
+  moon.Vx = moon.Vx + Fx / moon.Ma;
+  moon.Vy = moon.Vy + Fy / moon.Ma;
+  earth.Vx = earth.Vx - Fx / earth.Ma;
+  earth.Vy = earth.Vy - Fy / earth.Ma;
+  mars.Vx = mars.Vx - Fy / mars.Ma;
+  mars.Vx = mars.Vy - Fy / mars.Ma;
+
+  moon.x = moon.x + moon.Vx;
+  moon.y = moon.y + moon.Vy;
+  earth.x = earth.x + earth.Vx;
+  earth.y = earth.y + earth.Vy;
+  mars.x = mars.x + mars.Vx;
+  mars.y = mars.y + mars.Vy;
+  
+  moon.star.style.top = moon.y + 'px';
+  moon.star.style.left = moon.x + 'px';
+  
+  earth.star.style.top = earth.y + 'px';
+  earth.star.style.left = earth.x + 'px';
+
+  mars.star.style.top = mars.y + 'px';
+  mars.star.style.left = mars.x + 'px';
+  
+}
