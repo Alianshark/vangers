cbc4fa2e4b49029ccd3b0fbe176096cd7f845ad5
Enormouse <johnnysalvia@gmail.com>
2023-03-13 23:37:33 +0200

    add sun obj

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ let mars = {
   y: 100,
   Vx: 0,
   Vy: 0,
-  Ma: 1,
+  Ma: 1000,
   star: document.querySelector('#mars'),
+}
+
+let sun = {
+  x: 600,
+  y: 400,
+  Vx: 0,
+  Vy: 0,
+  Ma: 100000,
+  star: document.querySelector('#sun'),
 }
 
 let earth = {
@@ -17,9 +26,9 @@ let earth = {
 }
 let moon = {
   x: 400,
-  y: 300,
+  y: 400,
   Vx: 10, //speed
-  Vy: 0, //speed
+  Vy: -10, //speed
   Ma: 1, //Mass
   star: document.querySelector('#moon'),
 }
@@ -35,6 +44,7 @@ const G = 0.1; // G-constat
 mars.star.style.position = 'absolute';
 moon.star.style.position = 'absolute';
 earth.star.style.position = 'absolute';
+sun.star.style.position = 'absolute';
 
 setInterval(time, 1000/60);
 
@@ -50,8 +60,10 @@ function time () {
   moon.Vy = moon.Vy + Fy / moon.Ma;
   earth.Vx = earth.Vx - Fx / earth.Ma;
   earth.Vy = earth.Vy - Fy / earth.Ma;
-  mars.Vx = mars.Vx - Fy / mars.Ma;
+  mars.Vx = mars.Vx - Fx / mars.Ma;
   mars.Vx = mars.Vy - Fy / mars.Ma;
+  sun.Vx = sun.Vx - Fx / sun.Ma;
+  sun.Vy = sun.Vy - Fy / sun.Ma;
 
   moon.x = moon.x + moon.Vx;
   moon.y = moon.y + moon.Vy;
@@ -59,14 +71,16 @@ function time () {
   earth.y = earth.y + earth.Vy;
   mars.x = mars.x + mars.Vx;
   mars.y = mars.y + mars.Vy;
+  sun.x = sun.x + sun.Vx;
+  sun.y = sun.y + sun.Vy;
   
   moon.star.style.top = moon.y + 'px';
   moon.star.style.left = moon.x + 'px';
-  
   earth.star.style.top = earth.y + 'px';
   earth.star.style.left = earth.x + 'px';
-
   mars.star.style.top = mars.y + 'px';
   mars.star.style.left = mars.x + 'px';
-  
+  sun.star.style.top = sun.y + 'px';
+  sun.star.style.left = sun.x + 'px';
+  console.log(sun);
 }
